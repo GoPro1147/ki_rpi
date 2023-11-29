90b4f5e706cdf595f5cf9473f2de9704a780b818
GoPro1147 <cooth1147@gmail.com>
2023-11-29 14:41:53 +0900

    asdf

---
+++ app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, File
+from fastapi.responses import FileResponse
+
+from camera import takePicture
+
+app = FastAPI()
+
+# 이미지를 반환하는 엔드포인트
+@app.get("/get_image")
+async def get_image():
+
+    # 여기서는 예시로 이미지 파일 경로를 지정합니다. 실제로는 원하는 이미지 파일을 선택해야 합니다.
+    image_path = takePicture  # 이미지 파일 경로를 입력하세요.
+    print(image_path)
+    # 파일 응답 반환
+    return FileResponse(image_path)
