749fc7dc63d4c002cd538ab26af7bce0c485f967
GoPro1147 <cooth1147@gmail.com>
2023-11-29 21:29:50 +0900

    ㅁㄴㅇㄹㄹㅇㄴㅁ

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-import os
-
 from fastapi import FastAPI, File
 from fastapi.responses import FileResponse
-
+from fastapi import BackgroundTasks
+import os
 from camera import takePicture
-
 
 app = FastAPI()
 
-# 이미지를 반환하는 엔드포인트
-@app.get("/get_image")
-async def get_image():
+def delete_file(file_path: str):
+    if os.path.exists(file_path):  # 파일이 존재하는지 확인
+        os.remove(file_path)  # 파일 삭제
 
-    # 여기서는 예시로 이미지 파일 경로를 지정합니다. 실제로는 원하는 이미지 파일을 선택해야 합니다.
+@app.get("/get_image")
+async def get_image(background_tasks: BackgroundTasks):
     image_path = takePicture()  # 이미지 파일 경로를 입력하세요.
-    response = FileResponse(image_path)
-    os.remove(image_path)
+    
     # 파일 응답 반환
+    response = FileResponse(image_path)
+
+    # 파일 삭제를 백그라운드 태스크로 등록
+    background_tasks.add_task(delete_file, image_path)
+
     return response
